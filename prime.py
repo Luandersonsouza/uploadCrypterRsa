9c095c9da7f328b73061a89c965f636c2d447955
Luanderson <jlgs@ic.ufal.br>
2021-05-14 16:26:33 -0300

    primeiro commit

---
+++ prime.py
@@ -0,0 +1,16 @@
+"""
+Checagem dos números fornecidos pelo usário são primos  (prime.py)
+"""
+from math import sqrt
+ 
+def is_prime(n):
+    i = 2
+    cont = 0
+    while i < sqrt(n):
+        if n % i == 0:
+            cont += 1
+        i += 1
+    if cont < 2:
+        return True
+    else:
+        return False
